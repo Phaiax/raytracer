73e0e6a3a7d76f5117fa9c2b17695f82a27715ac
Phaiax <phaiax-git@invisibletower.de>
2022-10-30 23:35:20 +0100

    Chapter 9: Fuzzy Metal

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use crate::hittables::HitRecord;
-use crate::util::{Ray, Color, random_unit_vector, near_zero, reflect};
+use crate::util::{Ray, Color, random_unit_vector, near_zero, reflect, random_in_unit_sphere};
 use rand::rngs::SmallRng;
 
 pub trait Material : Send + Sync {
@@ -33,19 +33,20 @@ impl Material for Lambertian {
 
 pub struct Metal {
 	pub albedo: Color,
+	pub fuzz: f64,
 }
 
 impl Metal {
-	pub fn new(albedo: Color) -> Arc<dyn Material> {
-		Arc::new(Metal { albedo })
+	pub fn new(albedo: Color, fuzz: f64) -> Arc<dyn Material> {
+		Arc::new(Metal { albedo, fuzz })
 	}	
 }
 
 impl Material for Metal {
-    fn scatter(&self, ray: &Ray, rec: &HitRecord, _rng: &mut SmallRng) -> Option<(Color, Ray)> {
+    fn scatter(&self, ray: &Ray, rec: &HitRecord, rng: &mut SmallRng) -> Option<(Color, Ray)> {
         let reflected = reflect(&ray.direction().normalize(), &rec.normal);
         if reflected.dot(&rec.normal) > 0. {
-	        let scattered = Ray::new(rec.p, reflected);
+	        let scattered = Ray::new(rec.p, reflected + self.fuzz * random_in_unit_sphere(rng));
 	        Some((self.albedo, scattered))
         } else {
         	None
