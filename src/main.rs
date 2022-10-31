326c14f0a6c8358540ced4316549a34df84c53eb
Phaiax <phaiax-git@invisibletower.de>
2022-10-31 14:08:41 +0100

    Chapter 14: Final Scene

--- src/main.rs
+++ src/main.rs
@@ -34,11 +34,12 @@ use rand::prelude::Distribution;
 use rand::rngs::SmallRng;
 use rand::{Rng, SeedableRng};
 use rayon::prelude::*;
+use util::vec3_random;
 
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-const IMAGE_WIDTH: u32 = 400;
+const ASPECT_RATIO: f64 = 3.0 / 2.0;
+const IMAGE_WIDTH: u32 = 1200;
 const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-const SAMPLES_PER_PIXEL: u32 = 100;
+const SAMPLES_PER_PIXEL: u32 = 500;
 const MAX_DEPTH: u32 = 50;
 
 pub fn raytracer() {
@@ -46,35 +47,19 @@ pub fn raytracer() {
     let bar = ProgressBar::new(IMAGE_HEIGHT as u64);
 
     // World
+    let world = scene_chapter13();
 
-    let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
-    let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let material_left = Dielectric::new(1.5);
-    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 0.0);
-
-    let mut world = World::new();
-    world.add(Sphere::new(0.0, -100.5, -1.0, 100.0, &material_ground));
-    world.add(Sphere::new(0.0, 0.0, -1.0, 0.5, &material_center));
-    world.add(Sphere::new(-1.0, 0.0, -1.0, 0.5, &material_left));
-    world.add(Sphere::new(-1.0, 0.0, -1.0, -0.45, &material_left));
-    world.add(Sphere::new(1.0, 0.0, -1.0, 0.5, &material_right));
-
-    // let matte = Lambertian::new(Color::new(0.5, 0.7, 0.9));
-
-    // let mut world = World::new();
-    // world.add(Sphere::new(0., 0., -1., 0.5, &matte));
-    // world.add(Sphere::new(0., -100.5, -1., 100., &matte));
-
-    let lookfrom = Point3::new(3.0, 3.0, 2.0);
-    let lookat = Point3::new(0.0, 0.0, -1.0);
+    // Camera
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),
         20.0,
         ASPECT_RATIO,
-        2.0, // aperture
-        (lookfrom - lookat).magnitude(),
+        0.1, // aperture
+        10.0, // dist_to_focus
     );
 
     let range = 0..IMAGE_HEIGHT;
@@ -100,6 +85,73 @@ pub fn raytracer() {
     bar.finish();
 
     img.lock().unwrap().save("output.png").unwrap();
+}
+
+fn scene_chapter13() -> World {
+    let mut world = World::new();
+    let mut small_rng = SmallRng::seed_from_u64(23428359242 as u64);
+    let distr_0_1: Uniform<f64> = Uniform::new(0.0, 1.0);
+    let distr_0p5_1: Uniform<f64> = Uniform::new(0.5, 1.0);
+
+    let material_ground = Lambertian::new(Color::new(0.5, 0.5, 0.5));
+    world.add(Sphere::new(0.0, -1000.0, 0.0, 1000.0, &material_ground));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = distr_0_1.sample(&mut small_rng);
+            let center = Point3::new(
+                a as f64 + 0.9 * distr_0_1.sample(&mut small_rng),
+                0.2,
+                b as f64 + 0.9 * distr_0_1.sample(&mut small_rng),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).magnitude() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffse
+                    let albedo: Color = vec3_random(&distr_0_1, &mut small_rng)
+                        .component_mul(&vec3_random(&distr_0_1, &mut small_rng));
+                    let sphere_material = Lambertian::new(albedo);
+                    world.add(Sphere::new(center.x, center.y, center.z, 0.2, &sphere_material));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo: Color = vec3_random(&distr_0p5_1, &mut small_rng);
+                    let fuzz = distr_0_1.sample(&mut small_rng) / 2.0;
+                    let sphere_material = Metal::new(albedo, fuzz);
+                    world.add(Sphere::new(center.x, center.y, center.z, 0.2, &sphere_material));
+                } else {
+                    // glass
+                    let sphere_material = Dielectric::new(1.5);
+                    world.add(Sphere::new(center.x, center.y, center.z, 0.2, &sphere_material));
+                }
+            }
+        }
+    }
+
+    let material1 = Dielectric::new(1.5);
+    world.add(Sphere::new(0.0, 1.0, 0.0, 1.0, &material1));
+
+    let material2 = Lambertian::new(Color::new(0.4, 0.2, 0.1));
+    world.add(Sphere::new(-4.0, 1.0, 0.0, 1.0, &material2));
+
+    let material3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
+    world.add(Sphere::new(4.0, 1.0, 0.0, 1.0, &material3));
+
+    world
+}
+
+fn scene_tutorial() -> World {
+    let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
+    let material_left = Dielectric::new(1.5);
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 0.0);
+
+    let mut world = World::new();
+    world.add(Sphere::new(0.0, -100.5, -1.0, 100.0, &material_ground));
+    world.add(Sphere::new(0.0, 0.0, -1.0, 0.5, &material_center));
+    world.add(Sphere::new(-1.0, 0.0, -1.0, 0.5, &material_left));
+    world.add(Sphere::new(-1.0, 0.0, -1.0, -0.45, &material_left));
+    world.add(Sphere::new(1.0, 0.0, -1.0, 0.5, &material_right));
+    world
 }
 
 fn ray_color(ray: &Ray, world: &World, depth: u32, rng: &mut SmallRng) -> Color {
